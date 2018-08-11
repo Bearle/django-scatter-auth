4a1d6b75a0a8dddcc5c8f2667cb33aa0c9b945ec
Denis Bobrov <delneg@Yandex.ru>
2018-08-11 14:52:56 +0300

    Deleted login.html Renamed web3auth to scatterauth dir

--- example/example/urls.py
+++ example/example/urls.py
@@ -22,14 +22,14 @@ from django.views.generic import RedirectView, TemplateView
 
 def auto_login(request):
     if not request.user.is_authenticated:
-        return render(request, 'web3auth/autologin.html')
+        return render(request, 'scatterauth/autologin.html')
     else:
         return redirect('/')
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name='web3auth/home.html')),
+    url(r'^$', TemplateView.as_view(template_name='scatterauth/home.html')),
     url(r'^auto_login/', auto_login, name='autologin'),
     url(r'', include('scatterauth.urls')),
 ]
